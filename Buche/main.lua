9324a5cd6d9ee2ebc43e0d54efde323114aab82d
AlessandroPerrone <51334511+AlessandroPerrone@users.noreply.github.com>
2019-06-04 12:33:23 +0200

    Load striscia - Load lives and scores

--- Buche/main.lua
+++ Buche/main.lua
@@ -9,10 +9,30 @@ local background = display.newImageRect( "background.png", 700, 1100 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
+--Load striscia
+local striscia = display.newImageRect("striscia.png", 10, 80)
+striscia.x = display.contentCenterX
+striscia.y = display.contentCenterY
+
 --Adding physics
 local physics = require( "physics" )
 physics.start()
 physics.setGravity( 0, 0 )
+
+--Initialize variables
+local lives = 3
+local score = 0
+local died = false
+
+local livesText
+local scoreText
+
+-- Set up display groups
+local uiGroup = display.newGroup()
+
+--Display lives and score
+livesText = display.newText( uiGroup, "Lives: ".. lives, 180, 60, native.systemFont, 36)
+scoreText = display.newText( uiGroup, "Score: ".. score, 380, 60, native.systemFont, 36)
 
 --Load autobus
 local autobus = display.newImageRect("autobus.png", 550, 250)
@@ -44,3 +64,4 @@ local function moveAutobus(event)
 end
 
 autobus: addEventListener( "touch", moveAutobus)
+
