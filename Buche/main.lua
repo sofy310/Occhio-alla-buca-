5e43a00889bc24a6d2af76efcf1ddf8dcb59d856
sofy310 <49167242+sofy310@users.noreply.github.com>
2019-06-04 15:32:53 +0200

    Collisione con buca1 aggiunta

--- Buche/main.lua
+++ Buche/main.lua
@@ -143,6 +143,7 @@ local autobus = display.newImageRect("autobus.png", 550, 250)
 autobus.x = display.contentCenterX
 autobus.y = display.contentHeight-170
 physics.addBody(autobus, "dynamic", {radius = 40, isSensor = true})
+autobus.myName = "autobus"
 
 --Move the autobus
 local function moveAutobus(event)
@@ -179,66 +180,69 @@ local function createBuca()
     local newBuca1 = display.newImageRect("buca1.png", math.random(100, 200), math.random(100, 200))
     table.insert(bucheTable, newBuca1)
     physics.addBody(newBuca1, "dynamic", {radius = 40, bounce = 0})
+    newBuca1.myName = "buca1"
 
     local newBuca2 = display.newImageRect("buca2.png", math.random(100, 200), math.random(100, 200))
     table.insert(bucheTable, newBuca2)
     physics.addBody(newBuca2, "dynamic", {radius = 40, bounce = 0})
+    newBuca2.myName = "buca2"
 
     local newBuca3 = display.newImageRect("buca3.png", math.random(100, 200), math.random(100, 200))
     table.insert(bucheTable, newBuca3)
     physics.addBody(newBuca3, "dynamic", {radius = 40, bounce = 0})
+    newBuca3.myName = "buca3"
 
     local whereFrom = math.random(11)
 
     if ( whereFrom == 1 ) then
-        -- From the topLeft
+        -- buca1 From the topLeft
         newBuca1.x = display.contentCenterX -200
         newBuca1.y = -60
         newBuca1:setLinearVelocity(0, 100 )
   
     elseif ( whereFrom == 2 ) then
-        -- From the topCenter
+        -- buca1 From the topCenter
         newBuca1.x = display.contentCenterX
         newBuca1.y = -60
         newBuca1:setLinearVelocity(0, 100 )
   
     elseif ( whereFrom == 3 ) then
-        -- From the topRight
+        -- buca1 From the topRight
         newBuca1.x = display.contentCenterX +200
         newBuca1.y = -60
         newBuca1:setLinearVelocity(0, 100 )
     
     elseif ( whereFrom == 4 ) then
-        -- From the topRight
+        -- buca2 From the topLeft
         newBuca1.x = display.contentCenterX -96
         newBuca1.y = -60
         newBuca1:setLinearVelocity(0, 100 )
     
     elseif ( whereFrom == 5 ) then
-        -- From the topRight
+        -- buca2 From the topCenter
         newBuca1.x = display.contentCenterX +96
         newBuca1.y = -60
         newBuca1:setLinearVelocity(0, 100 )
     
     elseif ( whereFrom == 6 ) then
-        -- From the topRight
+        -- buca2 From the topRight
         newBuca2.x = display.contentCenterX +96
         newBuca2.y = -60
         newBuca2:setLinearVelocity(0, 100 )
     
     elseif ( whereFrom == 7 ) then
-        -- From the topRight
+        -- buca3 From the topLeft
         newBuca2.x = display.contentCenterX +96
         newBuca2.y = -60
         newBuca2:setLinearVelocity(0, 100 )
     
     elseif ( whereFrom == 8 ) then
-        -- From the topRight
+        -- buca3 From the topCenter
         newBuca2.x = display.contentCenterX +96
         newBuca2.y = -60
         newBuca2:setLinearVelocity(0, 100 )
     elseif ( whereFrom == 9 ) then
-        -- From the topRight
+        -- buca3 From the topRight
         newBuca3.x = display.contentCenterX +96
         newBuca3.y = -60
         newBuca3:setLinearVelocity(0, 100 )
@@ -276,3 +280,49 @@ local function gameLoop()
 end
 
 gameLoopTimer = timer.performWithDelay(3000, gameLoop, 0 )
+
+local function restoreAutobus()
+ 
+    autobus.isBodyActive = false
+    autobus.x = display.contentCenterX
+    autobus.y = display.contentHeight - 100
+ 
+    -- Fade in the autobus
+    transition.to( autobus, { alpha=1, time=4000,
+        onComplete = function()
+            autobus.isBodyActive = true
+            died = false
+        end
+    } )
+end
+
+local function onCollision( event )
+ 
+    if ( event.phase == "began" ) then
+ 
+        local obj1 = event.object1
+        local obj2 = event.object2
+        
+        if ( ( obj1.myName == "autobus" and obj2.myName == "buca1" ) or
+             ( obj1.myName == "buca1" and obj2.myName == "autobus" ) )
+        then
+            if ( died == false ) then
+                died = true
+
+                --Update lives
+                lives = lives -1
+                livesText = "Lives: " ..lives
+
+                if ( lives == 0 ) then
+                    display.remove( autobus )
+                else
+                    autobus.alpha = 0
+                    timer.performWithDelay( 1000, restoreAutobus )
+                end
+            end
+ 
+        end
+        
+    end
+end
+Runtime:addEventListener( "collision", onCollision )
